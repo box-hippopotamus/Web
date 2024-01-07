a7f82c8216e1fc5e1fbb994a3bec4726a3a8a802
“盒马盒马git config --global user.email “box-hippopotamus <2353151024@qq.com>
2024-01-07 13:44:46 +0800

    code

--- horse/JavaScript/18.jQuery-动画&节点/19-记事本/js/index.js
+++ horse/JavaScript/18.jQuery-动画&节点/19-记事本/js/index.js
@@ -1,42 +1,32 @@
 ;(function () {
-  // 3.删除
-  function count () {
-    // console.log($('#todoList li'))
-    $('.todo-count strong').text($('#todoList li').length)
-  }
-  // 1.新增
-  $('#addTodo').keyup(function (e) {
-    if (e.keyCode === 13) {
-      // console.log('回车')
-      let value = $(this).val()
-      if (value !== '') {
-        console.log('输入了内容')
-        $('#todoList').append(`
-        <li style="display:none">
+  //输入
+  $("#addTodo").keyup(function (e) {
+    if(e.keyCode === 13){
+      let value = $(this).val();
+
+      if(value !== ''){
+        $("#todoList").append(`
+        <li style="display: none">
             <div class="view">
               <label>${value}</label>
               <button class="destroy"></button>
             </div>
-          </li>`)
-        $('#todoList li')
-          .last()
-          .slideDown(function () {
-            count()
-          })
-        $(this).val('')
+          </li>
+        `)
+        $("#todoList li").last().slideDown(function () {
+          $(".todo-count strong").text($("#todoList li").length);
+        });
+        $(this).val("");
       }
     }
   })
 
-  // 2.删除
-  $('#todoList').on('click', '.destroy', function () {
-    let $li = $(this)
-      .parent()
-      .parent()
-    $li.fadeOut(function () {
-      $(this).remove()
-      // $('.todo-count strong').text($('#todoList li').length)
-      count()
-    })
+  //删除
+  $("#todoList").on("click", ".destroy", function() {
+    $(this).parent().parent().slideUp(function() {
+      $(this).remove();
+      $(".todo-count strong").text($("#todoList li").length);
+    });
   })
+  
 })()
